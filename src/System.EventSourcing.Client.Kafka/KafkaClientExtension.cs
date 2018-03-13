0706c9bf10deb24ae4b3d9877c28ed7ea5810f66
Michael (Enzian) Ingold <github@michaelingold.ch>
2018-03-13 14:29:20 +0100

    fixed memory leak from unclosed kafka producer

--- src/System.EventSourcing.Client.Kafka/KafkaClientExtension.cs
+++ src/System.EventSourcing.Client.Kafka/KafkaClientExtension.cs
@@ -14,11 +14,11 @@ namespace System.EventSourcing.Client.Kafka
         public static EventClient UseKafka(this EventClient subject, string topic, string bootstrapServers)
         {
             var config = new Dictionary<string, object> { { "bootstrap.servers", bootstrapServers } };
-            var producer = new Producer<string, string>(config, new StringSerializer(Encoding.UTF8), new StringSerializer(Encoding.UTF8));
 
             subject.UseHandle(
                 async evnt =>
                 {
+                    var producer = new Producer<string, string>(config, new StringSerializer(Encoding.UTF8), new StringSerializer(Encoding.UTF8));
                     var kafkaEvent = new KafkaEvent { Tags = evnt.Tags, Content = JToken.Parse(evnt.Content)};
                     var strContent = await Task.Run(() => JsonConvert.SerializeObject(kafkaEvent));
                     var message = await producer.ProduceAsync(topic, evnt.Name, strContent);
